efcf163e81c4597536e50a7367f1523da521a15c
Jack Hassett <jack.hassett@ad.aie.edu>
2016-06-08 18:02:22 +0930

    Game

---
+++ Platformer/Platformer/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    class Player
+    {
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+
+        }
+        public void Update (float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "hero");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
